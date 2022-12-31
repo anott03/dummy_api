acea277c6cfeb48d515d875a21c39c4c894c17ed
Amitav Nott <58123848+anott03@users.noreply.github.com>
2022-12-31 02:09:33 -0500

    KV is working!

--- src/lib.rs
+++ src/lib.rs
@@ -13,42 +13,93 @@ fn log_request(req: &Request) {
     );
 }
 
+async fn get_value(mut req: Request, ctx: RouteContext<()>) -> std::result::Result<worker::Response, worker::Error> {
+    let data: serde_json::Value = match req.json().await {
+        Ok(d) => d,
+        Err(_) => { return Response::error("Bad request", 400); }
+    };
+    let key = match data.get("key") {
+        Some(n) => n.to_string(),
+        None => {
+            return Response::error("Bad Request: Missing key", 400);
+        },
+    };
+    let kv = ctx.kv("DUMMY_API")?;
+    let value = match kv.get(&key).json::<String>().await {
+        Ok(v) => v,
+        Err(_err) => Some(String::new())
+    };
+
+    return match value {
+        Some(val) => Response::from_json(&json!({ "success": true, "value": val.as_str() })),
+        None => Response::from_json(&json!({ "success": false, "message": format!("no value fouund for key {}", key) }))
+    };
+}
+
+async fn add_value(mut req: Request, ctx: RouteContext<()>) -> std::result::Result<worker::Response, worker::Error> {
+    let data: serde_json::Value = match req.json().await {
+        Ok(d) => d,
+        Err(_) => { return Response::error("Bad request", 400); }
+    };
+    let key = match data.get("key") {
+        Some(n) => n,
+        None => {
+            return Response::error("Bad Request: Missing key", 400);
+        },
+    };
+    let value = match data.get("value") {
+        Some(n) => n,
+        None => {
+            return Response::error("Bad Request: Missing value", 400);
+        },
+    };
+
+    let kv = ctx.kv("DUMMY_API")?;
+    kv.put(&key.to_string(), &value.to_string())?
+        .execute()
+        .await?;
+
+    return Response::from_json(&json!({ "success": true }));
+}
+
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
     log_request(&req);
-
-    // Optionally, get more helpful error messages written to the console in the case of a panic.
     utils::set_panic_hook();
 
-    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
-    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
-    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
-    let router = Router::new();
+    // do middlware stuff here, then if the request is valid we can pass it to the router
+    let req_is_valid: bool = true;
+    if !req_is_valid {
+        return Response::error("Invalid Request", 400);
+    }
 
-    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
-    // functionality and a `RouteContext` which you can use to  and get route parameters and
-    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
+    let router = Router::new();
     router
         .get("/", |_, _| Response::ok("Hello from Workers!"))
-        .post_async("/form/:field", |mut req, ctx| async move {
-            if let Some(name) = ctx.param("field") {
-                let form = req.form_data().await?;
-                match form.get(name) {
-                    Some(FormEntry::Field(value)) => {
-                        return Response::from_json(&json!({ name: value }))
-                    }
-                    Some(FormEntry::File(_)) => {
-                        return Response::error("`field` param in form shouldn't be a File", 422);
-                    }
-                    None => return Response::error("Bad Request", 400),
+        .get_async("/hello", |mut req, _ctx| async move {
+            let data: serde_json::Value = match req.json().await {
+                Ok(d) => d,
+                Err(_) => {
+                    return Response::error("Bad Request: No json", 400);
                 }
-            }
-
-            Response::error("Bad Request", 400)
+            };
+            let name = match data.get("name") {
+                Some(n) => n,
+                None => {
+                    return Response::error("Bad Request: Missing name", 400);
+                },
+            };
+            return Response::from_json(&json!({ "message": format!("hello, {}!", name) }));
         })
         .get("/worker-version", |_, ctx| {
             let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
             Response::ok(version)
+        })
+        .post_async("/add", |req, ctx| async move {
+            return add_value(req, ctx).await;
+        })
+        .get_async("/get", |req, ctx| async move {
+            return get_value(req, ctx).await;
         })
         .run(req, env)
         .await
